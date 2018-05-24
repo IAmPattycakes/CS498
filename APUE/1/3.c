650381b642a1cd66d93f884e2dbaef5d565a5337
IAmPattycakes <contact@patrickgroh.com>
2018-05-24 12:20:32 -0400

    Updated answer for exercise 1.3

--- APUE/1/3.c
+++ APUE/1/3.c
@@ -1,2 +1,8 @@
 /*
-perror sounds like it has been a 
+perror sounds like it uses a constant because the error message that it
+is recieving needs to never change for clarity, as it sounds like that
+is what would go into a file for error logging. strerror is an actual 
+map function it sounds, meaning it would get any number for the input,
+dynamic or not, and map it to a string that would provide the full error
+message. 
+*/
