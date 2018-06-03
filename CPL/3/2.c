1d021b6916b718e0658fdf0f6ffb909d2c75f9f3
IAmPattycakes <contact@patrickgroh.com>
2018-06-03 11:37:12 -0400

    Fixed CPL 3.2

--- CPL/3/2.c
+++ CPL/3/2.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 
 void escape(char s[], char t[]);
+char testString[] = "THis is a\ttest of \ttabs and \n some \n newlines";
+
+char outputString[100];
 
 int main()
 {
-
+	printf("%s\n\n\n", testString);
+	escape(outputString, testString);
+	printf("%s\n\n\n", outputString);
 }
 
 void escape(char s[], char t[])
 {
 	int i, j;
-	i = j = 0;
-	while((char c = t[i]) != '\0') {
+	i = 0;
+	j = 0;
+	char c;
+	while((c = t[i]) != '\0') {
 		switch (c) {
-			case '\n':
-			case '\t':
-			default:
+			case '\n': s[j++] = '\\'; s[j] = 'n'; break;
+			case '\t': s[j++] = '\\'; s[j] = 't'; break;
+			//case '\n': s[j] = "\\n"; break;
+			//case '\t': s[j] = "\\t"; break;
+			default: s[j] = c;
 		}
 		i++;
 		j++;
